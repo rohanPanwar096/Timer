7061a8fcf733245e70a20fd0eb30e273e2144041
Rohan Panwar <rohan.panwar@neurontechlabs.com>
2020-12-19 03:11:12 +0530

    fixed small errors

--- src/Timer.js
+++ src/Timer.js
@@ -24,12 +24,20 @@ export default function Timer({seconds}) {
     }
 
     useEffect(() => {
-        setSecondsLeft(seconds)
+        if(seconds > 0) {
+            setSecondsLeft(seconds)
+        }
+
+        if(seconds === 0) {
+            setSecondsLeft(seconds)
+        }
     }, [seconds])
 
     useEffect(() => {
-        let timeLeft = secondsToTime(secondsLeft);
-        setTime(timeLeft);
+        if(secondsLeft >= 0) {
+            let timeLeft = secondsToTime(secondsLeft);
+            setTime(timeLeft);
+        }
 
     }, [secondsLeft])
 
@@ -37,7 +45,7 @@ export default function Timer({seconds}) {
         let interval = null
         if (is_active) {
           interval = setInterval(() => {
-                setSecondsLeft(s => s -1)
+                setSecondsLeft((s) => s - 1)
             }, 1000)
         } else {
             clearInterval(interval)
@@ -50,16 +58,16 @@ export default function Timer({seconds}) {
     return (
         <div className="flex flex-col items-center">
             <h1>
-                <span className="mr-1">{seconds ? time["h"] + " : " : "HH :"}</span>
-                <span className="ml-1">{seconds ? time["m"] + " : " : "MM :"}</span>
-                <span className="ml-1">{seconds ? time["s"] : "SS"}</span>
+                <span className="mr-1" id="hrs">{seconds ? time["h"] + " : " : "HH :"}</span>
+                <span className="ml-1" id="mins">{seconds ? time["m"] + " : " : "MM :"}</span>
+                <span className="ml-1" id="secs">{seconds ? time["s"] : "SS"}</span>
             </h1>
             <div className="block mt-6">
                 <button onClick={() => setActive(true)}  className="focus:outline-none px-4 py-2 mx-3 hover:bg-blue-600 rounded-lg bg-blue-300 border-2 text-sm font-lg ">Start</button> {" "}
                 <button onClick={() => setActive(false)} className="focus:outline-none px-4 py-2 hover:bg-red-600 rounded-lg bg-red-300 border-2 text-sm font-lg">Stop</button>
                 <button onClick={() => setTime(() => {
                     setActive(false);
-                    document.getElementById("min").value = 0 
+                    document.getElementById("min").value = 0
                     return {h: 0, m: 0, s: 0}
                 })} className="focus:outline-none px-4 py-2 ml-3 hover:bg-gray-600 rounded-lg bg-gray-300 border-2 text-sm font-lg">Reset</button>
             </div>
